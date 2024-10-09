310fbc77173ca46f41941eb89c7bbcba8222040a
AsierCL <asiercl.05@gmail.com>
2024-10-09 17:26:10 +0200

    Comentarios y cambios menores de interfaz

--- include/got.h
+++ include/got.h
@@ -3,10 +3,31 @@
 
 #include "../include/abb.h"
 
+/**
+ * Añade un nuevo personaje al árbol de personajes [arbol].
+ * Se solicitan al usuario los datos del personaje (nombre, casa, estatus real, padres, hermanos, personajes asesinados)
+ * y se inserta en el árbol binario de búsqueda.
+ * 
+ * @param arbol Puntero al árbol donde se va a insertar el personaje.
+ */
 void anhadirPersonaje(TABB *arbol);
 
+/**
+ * Lista todos los personajes del árbol [arbol] en orden ascendente de acuerdo a sus nombres.
+ * El recorrido del árbol es en orden (IRD: izquierda-raíz-derecha), mostrando el nombre,
+ * casa, si es miembro de la realeza, padres, hermanos y personajes asesinados.
+ *
+ * @param arbol Árbol de personajes a listar.
+ */
 void listarPersonajes(TABB arbol);
 
+/**
+ * Elimina un personaje del árbol de personajes [arbol].
+ * Solicita al usuario el nombre del personaje a eliminar y lo elimina del árbol binario de búsqueda.
+ * No se contempla la situación en la que el personaje no exista en el árbol.
+ *
+ * @param arbol Puntero al árbol del cual se eliminará el personaje.
+ */
 void eliminarPersonaje(TABB *arbol);
 
 #endif
--- src/got.c
+++ src/got.c
@@ -4,72 +4,79 @@
 
 #include "../include/abb.h"
 
-
 void anhadirPersonaje(TABB *arbol){
-    //char input[MAX]={0};
+    // Variables
     TIPOELEMENTOLISTA input;
     input.nameP[0] = '\0';
     TIPOELEMENTOABB personaje;
+
+    // Solicita datos del personaje
     printf("\nIntroduce el nombre del personaje: ");
-    scanf(" %[^\n\r]",personaje.name);
+    scanf(" %[^\n\r]", personaje.name);
 
     printf("\nCasa (? si es desconocida): ");
-    scanf(" %[^\n\r]",personaje.house);
+    scanf(" %[^\n\r]", personaje.house);
     
     printf("\nRoyal (0/1): ");
-    scanf("%d",&personaje.royal);
+    scanf("%d", &personaje.royal);
     
+    // Lista de padres
     crearLista(&personaje.parents);
     while(strcmp(input.nameP, "fin")){
         printf("\nParents (fin para terminar): ");
-        scanf(" %[^\n\r]",input.nameP);
+        scanf(" %[^\n\r]", input.nameP);
         if(strcmp(input.nameP, "fin")){
             insertarElementoLista(&personaje.parents, finLista(personaje.parents), input);
         }
     }
     input.nameP[0] = '\0';
 
+    // Lista de hermanos
     crearLista(&personaje.siblings);
     while(strcmp(input.nameP, "fin")){
         printf("\nSiblings (fin para terminar): ");
-        scanf(" %[^\n\r]",input.nameP);
+        scanf(" %[^\n\r]", input.nameP);
         if(strcmp(input.nameP, "fin")){
             insertarElementoLista(&personaje.siblings, finLista(personaje.siblings), input);
         }
     }
     input.nameP[0] = '\0';
 
+    // Lista de asesinados
     crearLista(&personaje.killed);
     while(strcmp(input.nameP, "fin")){
         printf("\nKilled (fin para terminar): ");
-        scanf(" %[^\n\r]",input.nameP);
+        scanf(" %[^\n\r]", input.nameP);
         if(strcmp(input.nameP, "fin")){
             insertarElementoLista(&personaje.killed, finLista(personaje.killed), input);
         }
     }
     input.nameP[0] = '\0';
 
+    // Inserta personaje en el árbol
     insertarElementoAbb(arbol, personaje);
-
 }
 
 void imprimirLista(TLISTA lista){
-    //Elementos
+    // Variables
     TIPOELEMENTOLISTA elemento;
     TPOSICION posicion = primeroLista(lista);
     
-    for(int i=0;i<longitudLista(lista);i++){
+    // Recorre e imprime la lista
+    for(int i = 0; i < longitudLista(lista); i++){
         recuperarElementoLista(lista, posicion, &elemento);
         posicion = siguienteLista(lista, posicion);
-        printf("%s, ",elemento.nameP);
+        printf("%s, ", elemento.nameP);
     }
 }
 
 void imprimirPersonaje(TIPOELEMENTOABB personaje){
-    //Nombre
-    printf("%s\n",personaje.name);
-    printf("\tHouse: \t%s\n",personaje.house);
-    printf("\tRoyal\t%d\n",personaje.royal);
+    // Imprime los datos del personaje
+    printf("\n%s\n", personaje.name);
+    printf("\tHouse: \t%s\n", personaje.house);
+    printf("\tRoyal\t%d", personaje.royal);
+
+    // Imprime listas asociadas (padres, hermanos, asesinados)
     printf("\n\tParents: ");
     imprimirLista(personaje.parents);
     printf("\n\tSiblings: ");
@@ -79,18 +86,7 @@ void imprimirPersonaje(TIPOELEMENTOABB personaje){
     printf("\n");
 }
 
-/* void listarPersonaje(TABB *arbol){
-    TIPOELEMENTOABB personaje;
-    char input[MAX];
-    printf("Introduce el nombre del personaje que quieres buscar: ");
-    scanf(" %s",input);
-
-    buscarNodoAbb(*arbol, input, &personaje);
-
-    imprimirPersonaje(personaje);
-} */
-
-void listarPersonajes(TABB arbol){
+void listarPersonajes(TABB arbol){ // Recorrido IRD (inorden)
     if(!esAbbVacio(arbol)){
         TIPOELEMENTOABB elemento;
         listarPersonajes(izqAbb(arbol));
@@ -101,12 +97,15 @@ void listarPersonajes(TABB arbol){
 }
 
 void eliminarPersonaje(TABB *arbol){
+    // Variables
     TIPOELEMENTOABB personaje;
     char input[MAX];
+
+    // Solicita nombre del personaje a eliminar
     printf("Introduce el nombre del personaje que quieres eliminar: ");
-    scanf(" %s",input);
+    scanf(" %s", input);
 
+    // Busca y elimina el personaje
     buscarNodoAbb(*arbol, input, &personaje);
-
     suprimirElementoAbb(arbol, personaje);
 }
