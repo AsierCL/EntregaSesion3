070b806b8c4029901d0d1b0ac36ddc5096711f66
AsierCL <asiercl.05@gmail.com>
2024-10-09 10:05:32 +0200

    Anhadir personaje implementado

--- src/got.c
+++ src/got.c
@@ -6,7 +6,9 @@
 
 
 void anhadirPersonaje(TABB *arbol){
-    char input[MAX]={0};
+    //char input[MAX]={0};
+    TIPOELEMENTOLISTA input;
+    input.nameP[0] = '\0';
     TIPOELEMENTOABB personaje;
     printf("\nIntroduce el nombre del personaje: ");
     scanf(" %[^\n\r]",personaje.name);
@@ -15,10 +17,28 @@ void anhadirPersonaje(TABB *arbol){
     scanf(" %[^\n\r]",personaje.house);
     
     crearLista(&personaje.parents);
-    while(strcmp(input, "fin")){
+    while(strcmp(input.nameP, "fin")){
         printf("\nParents (fin para terminar): ");
-        scanf(" %[^\n\r]",input);
+        scanf(" %[^\n\r]",input.nameP);
+        insertarElementoLista(&personaje.parents, finLista(personaje.parents), input);
     }
+    input.nameP[0] = '\0';
+
+    crearLista(&personaje.siblings);
+    while(strcmp(input.nameP, "fin")){
+        printf("\nSiblings (fin para terminar): ");
+        scanf(" %[^\n\r]",input.nameP);
+        insertarElementoLista(&personaje.siblings, finLista(personaje.siblings), input);
+    }
+    input.nameP[0] = '\0';
+
+    crearLista(&personaje.killed);
+    while(strcmp(input.nameP, "fin")){
+        printf("\nKilled (fin para terminar): ");
+        scanf(" %[^\n\r]",input.nameP);
+        insertarElementoLista(&personaje.killed, finLista(personaje.killed), input);
+    }
+    input.nameP[0] = '\0';
 
     insertarElementoAbb(arbol, personaje);
 
